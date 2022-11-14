17ec1c820aa9588a0ff8a162b09315b352b5a80c
Pohorielova <107413244+Pohorielova@users.noreply.github.com>
2022-11-14 14:17:10 +0000

    Deploying to gh-pages from @ Pohorielova/goit-js-hw-09@124c1e95fb79fcdeb6a123af660c497e427b5ece 🚀

---
+++ 01-color-switcher.2d677d25.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let o=null;t.addEventListener("click",(function(){o=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.setAttribute("disabled",!0)}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.2d677d25.js.map
